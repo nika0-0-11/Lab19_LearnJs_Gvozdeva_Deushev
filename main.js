c1f8acb25223f252fdfddf605ded79b34d4eebfa
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-03-20 11:35:25 +0300

    Step 6: functions in JavaScript

--- main.js
+++ main.js
@@ -75,3 +75,102 @@
 //     console.log(sym);
 // }
 
+// Объявление функции (классический способ)
+
+// function sum (a, b) {
+//     return a + b;
+// }
+
+// console.log(sum(3, 5));
+
+// Практическое задание №1
+
+// function multiply(x, y) {
+//     return x * y;
+// }
+
+// console.log(multiply(5, 3));
+
+// function sayHello(name) {
+//     console.log(`Hello, ${name}`);
+// }
+
+// sayHello("Nika");
+
+// function printInfo(name, age) {
+//     console.log(`Name: ${name} \nAge: ${age}`);
+// }
+
+// printInfo("Nika", 18);
+
+// Значения параметров по умолчанию
+
+// function greet(name = "Гость") {
+//     console.log("Привет, " + name);
+// }
+
+// greet();
+// greet("Nika");
+
+// Практическое задание №3
+
+// function calculateDiscount(price, discount = 10) {
+//     return (price - (price * discount) / 100); 
+// }
+
+// console.log(calculateDiscount(100));
+// console.log(calculateDiscount(100, 50))
+
+// Функции как значения
+
+// const add = function (a, b) {
+//     return a + b;
+// };
+// console.log(add(2, 3));
+
+// // Пример замыканий (closure):
+
+// function makeCounter() {
+//     let count = 0;
+//     return function () {
+//         count++;
+//         return count;
+//     };
+// }
+
+// const counter = makeCounter();
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
+
+// Стрелочные функции (Arrow Functions)
+
+// Обычная функция:
+
+function sumFunc(a, b) {
+    return a + b;
+
+}
+
+// Стрелочная функция:
+
+const sumFunc2 = (a, b) => a + b;
+
+// Правила:
+// 1. Один параметр — скобки необязательны:
+const double = x => x * 2;
+
+// 2. Несколько параметров — скобки обязательны:
+const sumFunc3 = (a, b) => a +b;
+
+// 3. Нет параметров — пустые скобки:
+const sayGreeting = () => console.log("Hello");
+
+// 4. Одна строка — return неявный:
+const square = x => x * x;
+
+// 5. Несколько строк — фигурные скобки + явный return:
+const calculate = (a, b) => {
+    let result = a + b;
+    return result * 2;
+}
