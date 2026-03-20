0cf214d720c0ac5e103b458665ac5853d0525edf
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-03-20 11:09:55 +0300

    Step 5: loops in JavaScript

---
+++ main.js
@@ -0,0 +1,77 @@
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+//  Практическое задание №1
+
+// for (let count = 0; count < 10; count++) {
+//     if (count % 2 == 0) {
+//         console.log(count);
+//     }
+// }
+
+//Цикл while
+// let count = 0;
+
+// while (count < 3) {
+//     console.log("Count:", count);
+//     count++;
+// }
+
+// Практическое задание №2
+
+// let number = 5;
+
+// while (number > 0) {
+//     console.log("Number:", number);
+//     number--;
+// }
+
+// Цикл do...while
+
+// let doValue = 0;
+
+// do {
+//     console.log("Value:", doValue);
+//     doValue++;
+// } while (doValue < 3);
+
+// Операторы break и continue
+
+// for (let i = 0; i < 10; i++) {
+//     if (i === 5) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// Практическое задание №4
+
+// for (let i = 0; i < 10; i++) {
+//     if (i === 5) {
+//         continue;
+//     }
+//     else if (i === 8) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// Вложенные циклы
+
+// for (let i = 1; i <= 3; i++) {
+//     for (let j = 1; j <= 3; j++) {
+//         console.log(`i = ${i}, j = ${j}`);
+//     }
+// }
+
+// Практическое задание №5
+
+// for (i = 1; i <= 4; i++) {
+//     let sym = "";
+//     for (j = 1; j <= i; j++) {
+//         sym += "*";
+//     }
+//     console.log(sym);
+// }
+
